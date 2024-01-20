b7d066996dd889b7a2a8b6ee9b7e1d3dec0ef00c
Tamjid <tamjidhossen0x@gmail.com>
2024-01-20 13:41:10 +0600

    grid debugging added

--- Templates/DebuggingTemplate.cpp
+++ Templates/DebuggingTemplate.cpp
@@ -11,9 +11,11 @@ using namespace std;
 #ifndef ONLINE_JUDGE
 #define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
 #define debugin(x) cerr << #x <<" "; _print(x); cerr << "; ";
+#define debugGrid(x) cerr << #x <<" "; __print(x); cerr << endl;
 #else
 #define debug(x)
 #define debugin(x)
+#define debugGrid(x)
 #endif
 
 void _print(int t) {cerr << t;}void _print(string t) {cerr << t;}void _print(char t) {cerr << t;}
@@ -23,6 +25,8 @@ template <class T> void _print(vector <T> v);template <class T> void _print(set 
 template <class T, class V> void _print(map <T, V> v);template <class T> void _print(multiset <T> v);
 template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
 template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void __print(vector<vector<T>> v) {cerr << "\n"; for(auto i:v) { for(auto j:i) {_print(j); cerr << " ";}cerr << "\n";}}
+template <class T> void _print(deque <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
