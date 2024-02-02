c3d33e7c7d8b2badb2a28b60f770121be9df697e
Tamjid <tamjidhossen0x@gmail.com>
2024-02-02 18:15:02 +0600

    Template Update

--- Templates/DebuggingTemplate.cpp
+++ Templates/DebuggingTemplate.cpp
@@ -20,7 +20,7 @@ using namespace std;
 
 void _print(int t) {cerr << t;}void _print(string t) {cerr << t;}void _print(char t) {cerr << t;}
 void _print(lld t) {cerr << t;}void _print(double t) {cerr << t;}void _print(ull t) {cerr << t;}
-template <class T, class V> void _print(pair <T, V> p);
+template <class T, class V> void _print(pair <T, V> p); void _print(bool t) {cerr << t;}
 template <class T> void _print(vector <T> v);template <class T> void _print(set <T> v);
 template <class T, class V> void _print(map <T, V> v);template <class T> void _print(multiset <T> v);
 template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
